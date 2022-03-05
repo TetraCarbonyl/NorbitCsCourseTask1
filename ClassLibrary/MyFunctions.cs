245393fc3a1699a44fb81c4964589e6b14f29abb
User <User@DESKTOP-NMCMA6A>
2022-03-05 11:31:06 +0300

    Add project files.

---
+++ ClassLibrary/MyFunctions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MyFunctions
+    {
+        /// <summary>
+        /// Возвращает строку вида 1,2,3,...,N
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public string GetStringOfNumbers(int number)
+        {
+            if(number <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Значение доложно быть положительным", nameof(number));
+            }
+
+            var outputString = "";
+
+            for (var i = 1; i <= number; i++)
+            {
+                outputString += i.ToString();
+
+                if (i != number)
+                {
+                    outputString += ',';
+                }
+            }
+            return outputString;
+        }
+
+        /// <summary>
+        /// Возвращает строку в виде квадрата из звездочек размера N
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public string GetRectangleAsterix(int size)
+        {
+            if (size <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Размер квадрата должен быть положительным", nameof(size));
+            }
+            if (size%2 == 0)
+            {
+                throw new ArgumentOutOfRangeException("Размер квадрата должен быть нечётным", nameof(size));
+            }
+
+            var middle = (size - 1) / 2;
+
+            var outputString = "";
+
+            for (var i = 0; i < size; i++)
+            {
+                for (var j = 0; j < size; j++)
+                {
+                    outputString +=
+                        ((i != middle) || (j != middle))
+                            ? '*'
+                            : ' '; 
+                }
+                outputString += '\n';                
+            }
+            return outputString;
+        }
+    }
+}
---
+++ ConsoleUtilities/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using ClassLibrary;
+
+
+MyFunctions One = new MyFunctions();
+
+Console.WriteLine(One.GetStringOfNumbers(7));
+
+Console.WriteLine(One.GetRectangleAsterix(5));
